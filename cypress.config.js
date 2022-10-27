bacd858c48da4c5bc4f73c6212a5fafbc99adae3
Your Name <you@example.com>
2022-10-27 09:40:41 -0700

    27.11

--- cypress.config.js
+++ cypress.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require("cypress");
+const {addMatchImageSnapshotPlugin} = require ('cypress-image-snapshot/plugin');
 
 module.exports = defineConfig({
   e2e: {
@@ -7,3 +8,22 @@ module.exports = defineConfig({
     },
   },
 });
+
+module.exports = defineConfig({
+viewportHeight: 2250,
+viewportWidth: 1850,
+e2e: {
+  setupNodeEvents(on, config) {
+    addMatchImageSnapshotPlugin(on, congig);
+  },
+  excludeSpecPattern: ['**/cypress/e2e/1-getting-started', '**/cypress/e2e/1-advanced-examples'],
+  baseUrl: 'https://www.beaxy.com',
+  defaultCommandTimeout: 4000,
+  retries: 2,
+  watchForFileChanges: false,
+  screenshotOnRunFailure: false,
+  slowTestThreshold: 10000,
+}
+});
+
+
